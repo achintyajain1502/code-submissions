73c94970cf16961116f11f19e642d5ffb94ec723
Achintya Jain <achintyajain1502@gmail.com>
2026-05-22 15:56:59 +0530

    Time: 17 ms (27.10%) | Memory: 14.1 MB (92.82%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -6,8 +6,10 @@ class Solution(object):
         """
         for i in range(len(nums)):
             nums[i]=pow(nums[i],2)
-        # print(nums)
-        # for i in range(len(nums)):
-        #     nums[i]=nums[i]**2
         nums.sort()
         return(nums)
+
+        # for i in range(len(nums)):
+        #     nums[i]=nums[i]**2       (comparitively more time complexity)
+        # nums.sort()
+        # return(nums)
