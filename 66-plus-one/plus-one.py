4dd80236b87198351041b51ccc715c670b12a444
Achintya Jain <achintyajain1502@gmail.com>
2026-05-22 13:38:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (54.00%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -2,17 +2,24 @@ class Solution(object):
     def plusOne(self, digits):
         """
         :type digits: List[int]
-        :rtype: List[int]
-        """
-        f=0
-        if(digits.count(9)==len(digits)):
-            f=1
-        for i in range(len(digits)-1,-1,-1):
-            if(digits[i]==9):
-                digits[i]=0
-            else:
-                digits[i]+=1
-                break
-        if(f==1):
-            digits=[1]+digits
-        return(digits)
+        :rtype: List[int]"""
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
+
+        
+        # f=0
+        # if(digits.count(9)==len(digits)):
+        #     f=1
+        # for i in range(len(digits)-1,-1,-1):
+        #     if(digits[i]==9):
+        #         digits[i]=0
+        #     else:
+        #         digits[i]+=1
+        #         break
+        # if(f==1):
+        #     digits=[1]+digits
+        # return(digits)
