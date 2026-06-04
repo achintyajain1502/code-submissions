0cdac21a22c538e67d8fa475de6916020fca514c
Achintya Jain <achintyajain1502@gmail.com>
2026-06-04 19:50:53 +0530

    Time: 4 ms (14.67%) | Memory: 12.4 MB (58.68%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def commonFactors(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        c=1
+        for i in range(2,min(a,b)+1):
+            if a%i==0 and b%i==0:
+                c+=1
+        return c
+        
