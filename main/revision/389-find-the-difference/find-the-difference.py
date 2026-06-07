1a97db47a7a0bc10e273e93c22ee03c622623a63
Achintya Jain <achintyajain1502@gmail.com>
2026-06-07 22:31:18 +0530

    Time: 4 ms (45.22%) | Memory: 12.4 MB (65.59%) - LeetSync

---
+++ main/revision/389-find-the-difference/find-the-difference.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        k=0
+        l=0
+        s=list(s)
+        t=list(t)
+        for i in s:
+            k+=ord(i)
+        for i in t:
+            l+=ord(i)
+        return chr(l-k)
