7a7f12b8cc3cfc2a91e91b09589cc32895cf798c
Achintya Jain <achintyajain1502@gmail.com>
2026-07-13 15:33:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (20.09%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        n1=0
+        n2=0
+        for i in num1:
+            n1=n1*10+(ord(i)-ord("0"))
+        for i in num2:
+            n2=n2*10+(ord(i)-ord("0"))
+        return str(n1*n2)
+        
