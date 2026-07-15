7cc0deda4e1c6ed79dace814371ea36c71e57c82
Achintya Jain <achintyajain1502@gmail.com>
2026-07-15 22:33:01 +0530

    Time: 87 ms (31.15%) | Memory: 12.6 MB (43.07%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        def check(i, j):
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return check(i + 1, j) or check(i, j - 1)
+        return True
