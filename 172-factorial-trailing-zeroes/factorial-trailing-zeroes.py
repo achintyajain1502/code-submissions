3746eb728c1497f2428519e2b4e0ebed2e0b93b6
Achintya Jain <achintyajain1502@gmail.com>
2026-07-17 22:50:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (59.29%) - LeetSync

---
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ans=0
+        while n:
+            n//=5
+            ans+=n
+        return ans
