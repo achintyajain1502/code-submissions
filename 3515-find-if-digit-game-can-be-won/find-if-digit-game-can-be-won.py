d2e013f9c156f0be1d185c4a0c160afd7e33921c
Achintya Jain <achintyajain1502@gmail.com>
2026-07-27 14:36:13 +0530

    Time: 6 ms (11.92%) | Memory: 12.3 MB (52.91%) - LeetSync

---
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def canAliceWin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s1=0
+        s2=0
+        for i in nums:
+            if len(str(i))==2:
+                s2+=i
+            else:
+                s1+=i
+        if s1>s2 or s2>s1:
+            return True
+        return False        
